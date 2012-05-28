2678bb271c65290ca97e861eb81145c66ddc7c11
Clint Newsom <hcnewsom@gmail.com>
2012-05-28 12:55:09 -0400

    fixes to method calls

--- main.rb
+++ main.rb
@@ -1,21 +1,6 @@
-Dir["classes/*.rb"].each {|file| require file }
+Dir["classes/*.rb"].each { |file| require file }
+FAKERUN = true
 
-class Human < Unit; end
-class Soldier < Unit; end
-class Doctor < Unit; end
-
-class Dinosaur < Unit; end
-class VRaptor < Unit; end
-class TRex < Unit; end
-
-# stub classes
-class FakeGame
-	attr_accessor :map 
-end
-
-class FakePlayer 
-	attr_accessor :game
-end
 
 #instance vars
 terrain_key = {
@@ -26,18 +11,39 @@ terrain_key = {
 	'w' => Terrain.new("Water"),
 }
 
-# Let's try!!
-player = FakePlayer.new
-player.game = FakeGame.new
-player.game.map = Map.new terrain_key, <<-END 
-	gggggggggg
-	gggggggwww
-	ggggggwwff
-	gggppppppp
-	ggppggwfpf
-	ggpgggwwff
-END
 
-dixie = Unit.new(player, "Dixie")
-player.game.map.place(dixie, 0, 0)
-dixie.move(1, 0)
+if FAKERUN == true
+
+	class Human < Unit; end
+	class Soldier < Unit; end
+	class Doctor < Unit; end
+
+	class Dinosaur < Unit; end
+	class VRaptor < Unit; end
+	class TRex < Unit; end
+
+	# stub classes
+	class FakeGame
+		attr_accessor :map 
+	end
+
+	class FakePlayer 
+		attr_accessor :game
+	end
+
+	# init fake game classes
+	player = FakePlayer.new
+	player.game = FakeGame.new
+	player.game.map = Map.new terrain_key, <<-END 
+		gggggggggg
+		gggggggwww
+		ggggggwwff
+		gggppppppp
+		ggppggwfpf
+		ggpgggwwff
+	END
+
+	dixie = Unit.new(player, "Dixie")
+	player.game.map.place(0, 0, dixie)
+	player.game.map.move(0, 0, 1, 0)
+end
