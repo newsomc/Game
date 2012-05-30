b545f7543f72c88137dd47afd766e323d249688c
Clint Newsom <hcnewsom@gmail.com>
2012-05-30 08:36:47 -0400

    adding command line player class

--- classes/player.rb
+++ classes/player.rb
@@ -66,3 +66,46 @@ class DumbComputer < BasePlayer
 	end
 
 end
+
+require 'pp'
+class CLIPlayer < BasePlayer
+	def message(string)
+		puts string
+	end
+
+	def draw(map)
+		puts "Terrain"
+		pp map.terrain.map 
+		puts "Units:"
+		pp map.units.rep 
+	end
+
+	def do_choose(choices)
+		mapping = rep_mapping(choices)
+
+		choice = nil 
+
+		until choice 
+			puts "Choose: "
+			puts mapping.keys
+
+			print "Input: "
+			choice_key = STDIN.gets
+			choice = mapping[choice_key]
+
+			puts "Bad choice" unless choice
+
+			puts "Bad choice" unless choice
+		end
+
+		yield choice
+	end
+
+	def rep_mapping(data)
+		mapping = {}
+		data.each do |datum|
+			mapping[datum.rep.inspect] = datum
+		end
+		return mapping
+	end
+end
